c8c1562edb783d52dc4a01008e7f6c71f6900908
Etienne de V <edevalmont@gmail.com>
2025-04-30 17:57:34 +0200

    starting comments

--- models/facts.js
+++ models/facts.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
   author: { type: String, required: true, minlength: 2, maxlength: 30 },
   text: { type: String, required: true, minlength: 20, maxlength: 500 },
+  submittedAt: { type: Date, required: true },
 });
 
 const factSchema = new mongoose.Schema({
