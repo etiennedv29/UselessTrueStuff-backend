cc2dea2eda9449dbfe370d77fa351145a8bc084d
Etienne de V <edevalmont@gmail.com>
2025-05-10 23:29:43 +0200

    signin upet out all ok

--- models/users.js
+++ models/users.js
@@ -6,6 +6,7 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  token: { type: String, required: true },
   createdAt: Date,
 });
 
