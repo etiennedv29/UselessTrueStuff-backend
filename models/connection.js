538f615d078de88b8759091202e27e348d06ab3f
Etienne de V <edevalmont@gmail.com>
2025-07-12 22:41:50 +0200

    more timeout for mongodb server selction

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, { connectTimeoutMS: 4000 })
+  .connect(process.env.MONGODB_URI, {
+    connectTimeoutMS: 4000,
+    serverSelectionTimeoutMS: 30000,
+  })
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
