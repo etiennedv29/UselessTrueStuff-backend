df0128a5357d60d2218f37dde1b42c07beb30cf8
Etienne de V <edevalmont@gmail.com>
2025-09-04 16:04:23 +0200

    check

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 var express = require("express");
 var router = express.Router();
 
-const { signin, signup,findVotesByFactForUser,updateAccount,deleteAccount} = require("../controllers/users");
+const {
+  signin,
+  signup,
+  findVotesByFactForUser,
+  updateAccount,
+  deleteAccount,
+  resetPassword,
+} = require("../controllers/users");
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/:factId/:userId", findVotesByFactForUser);
-router.put("/updateAccount",updateAccount)
-router.post("/softDelete", deleteAccount)
+router.put("/updateAccount", updateAccount);
+router.post("/softDelete", deleteAccount);
+router.post("/resetPassword", resetPassword);
 
 module.exports = router;
